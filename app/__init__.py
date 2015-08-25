e35547801b494d8995b5a58e4730dbb38f4fda9f
zhang <zhang@localhost>
2015-08-25 18:19:07 +0800

    添加的第一个hello world 脚本

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+from app import views
---
+++ app/views.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#
+from app import app
+
+@app.route('/')
+
+@app.route('/index')
+
+def index():
+    return "Hello World!"
+
